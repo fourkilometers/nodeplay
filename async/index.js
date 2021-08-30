733aa48f8e3b2b84376238cfc0dca3d8959a8325
pangcongcong <pangcongcong@360.cn>
2021-08-30 16:04:26 +0800

    [async]: auto function

--- async/index.js
+++ async/index.js
@@ -25,6 +25,102 @@ function series(tasks) {
   });
 }
 
+function auto(tasks) {
+  let result = {};
+  const tasksLen = Object.keys(tasks).length;
+  const tsort_queue = [];
+
+  const indegrees = {}; //入度，记录每个任务依赖的前置任务
+  const edges = {}; //图的边，记录task流向
+  for (const key in tasks) {
+    const task = tasks[key];
+    if (task instanceof Array) {
+      indegrees[key] = task.length - 1;
+      for (let i = 0; i < task.length - 1; i++) {
+        const pre = task[i];
+        if (!edges[pre]) {
+          edges[pre] = [];
+        }
+        edges[pre].push(key);
+      }
+    } else if (typeof task === "function") {
+      tsort_queue.push(key);
+    } else {
+      throw new Error("Task '" + key + "' is not valid");
+    }
+  }
+
+  let tsSortIndex = 0;
+  const task_queue = tsort_queue.slice();
+  const indegreesCount = Object.assign({}, indegrees);
+
+  //先检测有没有闭环
+  while (tsort_queue.length > 0) {
+    const taskName = tsort_queue.pop();
+    tsSortIndex++;
+    edges[taskName] &&
+      edges[taskName].forEach((t) => {
+        indegreesCount[t]--;
+        if (indegreesCount[t] === 0) {
+          tsort_queue.push(t);
+        }
+      });
+  }
+
+  if (tsSortIndex !== tasksLen) {
+    throw new Error("auto cannot execute tasks due to a recursive dependency");
+  }
+
+  let remainTasks = tasksLen;
+  return new Promise((resolve, rejcet) => {
+    task_queue.forEach((taskName) => {
+      runTask(taskName, true);
+    });
+
+    function runTask(taskName, isStart) {
+      const taskFn =
+        typeof tasks[taskName] === "function"
+          ? tasks[taskName]
+          : tasks[taskName][tasks[taskName].length - 1];
+      try {
+        if (isStart) {
+          taskFn(iterateeCallback.bind(null, taskName));
+        } else {
+          taskFn(result, iterateeCallback.bind(null, taskName));
+        }
+      } catch (error) {
+        rejcet(error);
+      }
+    }
+
+    function iterateeCallback(taskName, error, value) {
+      result[taskName] = value;
+      remainTasks--;
+
+      if (remainTasks === 0) {
+        resolve(result);
+        return;
+      }
+
+      const nextTasks = edges[taskName];
+      if (!nextTasks) {
+        return;
+      }
+
+      nextTasks.forEach((nextTaskName) => {
+        indegrees[nextTaskName]--;
+
+        if (indegrees[nextTaskName] && indegrees[nextTaskName] > 0) {
+          return;
+        } else {
+          runTask(nextTaskName);
+        }
+      });
+    }
+  });
+}
+
 module.exports = {
   series,
+  auto,
 };
--- test/async.test.js
+++ test/async.test.js
@@ -1,6 +1,6 @@
-const { series } = require("../async");
+const { series, auto } = require("../async");
 const { describe, it } = require("mocha");
-const assert = require("assert");
+const assert = require("assert").strict;
 
 describe("series", () => {
   it("functions should run in given order", function (done) {
@@ -46,25 +46,66 @@ describe("series", () => {
   });
 });
 
-// series([
-//   function (callback) {
-//     setTimeout(() => {
-//       console.log("running task1");
+describe("auto", function () {
+  it("basic", function (done) {
+    const callOrder = [];
+    auto({
+      get_data: function (callback) {
+        setTimeout(() => {
+          callOrder.push("get_data");
+          callback(null, "data");
+        }, 100);
+      },
+      make_folder: function (callback) {
+        callOrder.push("make_folder");
+        callback(null, "folder");
+      },
+      write_file: [
+        "get_data",
+        "make_folder",
+        function (results, callback) {
+          callOrder.push("write_file");
+          callback(null, "filename");
+        },
+      ],
+      email_link: [
+        "write_file",
+        function (results, callback) {
+          callOrder.push("email_link");
+          callback(null, {
+            file: results.write_file,
+            email: "user@example.com",
+          });
+        },
+      ],
+    })
+      .then(function (results) {
+        assert.strictEqual(
+          JSON.stringify(callOrder),
+          JSON.stringify([
+            "make_folder",
+            "get_data",
+            "write_file",
+            "email_link",
+          ])
+        );
+        assert.strictEqual(
+          JSON.stringify(results),
+          JSON.stringify({
+            make_folder: "folder",
+            get_data: "data",
+            write_file: "filename",
+            email_link: {
+              file: "filename",
+              email: "user@example.com",
+            },
+          })
+        );
 
-//       callback("12", 1);
-//     }, 1000);
-//   },
-//   function (callback) {
-//     setTimeout(() => {
-//       console.log("running task2");
-
-//       callback(null, 2);
-//     }, 500);
-//   },
-// ])
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+        done();
+      })
+      .catch((e) => {
+        done(e);
+      });
+  });
+});
